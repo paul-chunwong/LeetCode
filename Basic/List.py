721674c928700667b5f8851057216d8eea6c3914
Chun Pang Wong <yoshipaulwong@gmail.com>
2022-07-03 04:52:31 +0100

    Added some basic about python

--- Basic/List.py
+++ Basic/List.py
@@ -1,1 +1,32 @@
-# asdas
+# List is a collection which is ordered and changeable. Allows duplicate members.
+
+
+thisList = ["apple", "banana", "cherry"]
+print(thisList)
+print(len(thisList))
+
+# Using List constructor to create List
+thislist = list(("apple", "banana", "cherry"))  # note the double round-brackets
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+# List slicing
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+    print("Yes, 'apple' is in the fruits list")
